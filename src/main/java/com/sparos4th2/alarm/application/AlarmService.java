cb5759aebc0f35c12f488739326f3b1c3632cecb
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-05-29 17:26:27 +0900

    feat: 알림을 10개를 기본적으로 호출할 수 있도록 했고 알림이 추가되면 실시간으로 추가 될 수 있도록 했음(#1)

--- src/main/java/com/sparos4th2/alarm/application/AlarmService.java
+++ src/main/java/com/sparos4th2/alarm/application/AlarmService.java
@@ -1,15 +1,18 @@
 package com.sparos4th2.alarm.application;
 
 import com.sparos4th2.alarm.domain.Alarm;
+import com.sparos4th2.alarm.dto.AlarmRequestDto;
 import com.sparos4th2.alarm.dto.AlarmResponseDto;
+import com.sparos4th2.alarm.vo.AlarmVo;
 import org.springframework.http.codec.ServerSentEvent;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface AlarmService {
 
-	void saveAlarm(String receiverUuid);
+	void saveAlarm();
 
-	Flux<AlarmResponseDto> getAlarm(String receiverUuid);
+	Flux<Alarm> getAlarm(String receiverUuid);
 
-	Flux<ServerSentEvent<Alarm>> subscribe(String receiverUuid);
+	Flux<Alarm> streamAlarms(String receiverUuid);
 }
