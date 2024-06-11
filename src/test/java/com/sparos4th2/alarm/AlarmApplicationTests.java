3b7700d5e1866dadb8896daa0da71ff73726a2c8
ddolboghi <rhdfyd128@gmail.com>
2024-06-11 12:35:48 +0900

    fix: 빌드 에러 방지 위한 주석처리

--- src/test/java/com/sparos4th2/alarm/AlarmApplicationTests.java
+++ src/test/java/com/sparos4th2/alarm/AlarmApplicationTests.java
@@ -3,7 +3,7 @@ package com.sparos4th2.alarm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AlarmApplicationTests {
 
 	@Test
