71940153f4dfb35d71b0b5d747cedfde422652e2
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-06-24 14:06:44 +0900

    feat:알림 vo수정(#36)

--- src/main/java/com/sparos4th2/alarm/vo/AlarmVo.java
+++ src/main/java/com/sparos4th2/alarm/vo/AlarmVo.java
@@ -1,5 +1,7 @@
 package com.sparos4th2.alarm.vo;
 
+import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -8,4 +10,13 @@ public class AlarmVo {
 	private String receiverUuid;
 	private String message;
 	private String eventType;
+	private LocalDateTime alarmTime;
+
+	@Builder
+	public AlarmVo(String receiverUuid, String message, String eventType, LocalDateTime alarmTime) {
+		this.receiverUuid = receiverUuid;
+		this.message = message;
+		this.eventType = eventType;
+		this.alarmTime = alarmTime;
+	}
 }
