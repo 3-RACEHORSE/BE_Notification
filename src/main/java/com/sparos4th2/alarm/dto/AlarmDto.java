d2367a259ce7cedc5fd35fc7996eb6280e81c786
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-06-24 14:12:11 +0900

    feat:dto를 빌더 선언(#36)

--- src/main/java/com/sparos4th2/alarm/dto/AlarmDto.java
+++ src/main/java/com/sparos4th2/alarm/dto/AlarmDto.java
@@ -2,6 +2,7 @@ package com.sparos4th2.alarm.dto;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,10 +10,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class AlarmDto {
 
 	private List<String> receiverUuids;
 	private String message;
 	private String eventType;
+
+	@Builder
+	public AlarmDto(List<String> receiverUuids, String message, String eventType) {
+		this.receiverUuids = receiverUuids;
+		this.message = message;
+		this.eventType = eventType;
+	}
 }
